32192e3818328b96c7843ce8cc1396c7d511449d
kendall <kendall@thekendall.io>
2023-04-29 23:45:27 -0400

    Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-//import './App.css'
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className='head-text'>React App</h1>
-    </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas/>
+      <Customizer />
+    </main>
   )
 }
 
